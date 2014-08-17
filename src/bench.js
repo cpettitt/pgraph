4a548c66ccae119d3633c41777acddd4f40a656c
Chris Pettitt <cpettitt@gmail.com>
2014-08-16 19:36:28 -0700

    More work on benchmarks

--- src/bench.js
+++ src/bench.js
@@ -1,31 +1,98 @@
 #!/usr/bin/env node
 
-var Suite = require("benchmark").Suite;
-
-var Digraph = require("..").Digraph;
+var Benchmark = require("benchmark"),
+    Suite = Benchmark.Suite,
+    sprintf = require("sprintf").sprintf;
 
 var suite = new Suite("benchmarks");
-suite.on("cycle", function(event) {
-  console.log("    " + event.target);
+suite.on("cycle", function(bench) {
+  var target = bench.target,
+      hz = target.hz,
+      stats = target.stats,
+      rme = stats.rme,
+      samples = stats.sample.length,
+      msg = sprintf("    %20s: %13s ops/sec \xb1 %s%% (%3d run(s) sampled)",
+                    target.name,
+                    Benchmark.formatNumber(hz.toFixed(2)),
+                    rme.toFixed(2),
+                    samples);
+  console.log(msg);
 });
 suite.on("error", function(event) {
   console.error("    " + event.target.error);
 });
 
-suite.add("set", function() {
-  Digraph.empty().set(0);
+var Digraph = require("..").Digraph;
+
+var EDGE_DENSITY = 0.2,
+    KEY_SIZE = 10;
+
+function keys(count) {
+  var ks = [],
+      k;
+  for (var i = 0; i < count; ++i) {
+    k = "";
+    for (var j = 0; j < KEY_SIZE; ++j) {
+      k += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    }
+    ks.push(k);
+  }
+  return ks;
+}
+
+function buildGraph(numNodes, edgeDensity) {
+  var g = Digraph.empty(),
+      numEdges = numNodes * numNodes * edgeDensity,
+      ks = keys(numNodes);
+  ks.forEach(function(k) {
+    g = g.set(k);
+  });
+
+  for (var i = 0; i < numEdges; ++i) {
+    var v, w;
+    do {
+      v = ks[Math.floor(Math.random() * ks.length)];
+      w = ks[Math.floor(Math.random() * ks.length)];
+    } while (g.hasEdge(v, w));
+    g = g.setEdge(v, w);
+  }
+  return g;
+}
+
+suite.add("constructor", function() {
+  new Digraph();
 });
 
-suite.add("setDelete", function() {
-  Digraph.empty().set(0).delete(0);
-});
+[10, 100, 1000].forEach(function(size) {
+  var g = buildGraph(size, EDGE_DENSITY),
+      nodeIds = g.nodeIds(),
+      node = nodeIds.get(Math.floor(Math.random() * nodeIds.length)),
+      edges = g.edges(),
+      edge = edges.get(Math.floor(Math.random() * edges.length));
 
-suite.add("setEdge", function() {
-  Digraph.empty().setEdge(0, 1);
-});
+  suite.add("set(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.set("mykey", "label");
+  });
 
-suite.add("setDeleteEdge", function() {
-  Digraph.empty().setEdge(0, 1).deleteEdge(0, 1);
+  suite.add("get(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.get(node);
+  });
+
+  suite.add("delete(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.delete(node);
+  });
+
+  suite.add("setEdge(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.setEdge("from", "to", "label");
+  });
+
+  suite.add("getEdge(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.getEdge(edge.get("v"), edge.get("w"));
+  });
+
+  suite.add("deleteEdge(" + size + "," + EDGE_DENSITY + ")", function() {
+    g.deleteEdge(edge.v, edge.w);
+  });
 });
 
 suite.run();
